f7be2be35cd6063e9094a3985fe12e57f8bc6ac4
Tim <fielder_t@utpb.edu>
2024-09-30 15:23:33 -0500

    Initial copy of old Flappy Bird codebase to new repo.

---
+++ src/Game.java
@@ -0,0 +1,423 @@
+import javax.imageio.ImageIO;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+import java.io.*;
+
+public class Game implements Runnable
+{
+    private final GameCanvas canvas;
+
+    private final double rateTarget = 100.0;
+    public double waitTime = 1000.0 / rateTarget;
+    public double rate = 1000 / waitTime;
+
+    public Bird bird;
+    public int mouseX;
+    public int mouseY;
+    public int fireRate = 10;
+    public int fireCounter = 0;
+    public boolean firing = false;
+
+    public Pipe[] pipes = new Pipe[5];
+    private int pipeCount = 1;
+
+    public int highScore = 0;
+    public int score = 0;
+
+    public Toolkit tk;
+    public boolean debug = false;
+    public boolean running = true;
+    public double volume = 0.3;
+    public boolean randomGaps = false;
+    public double difficulty = 0.0;
+    public boolean ramping = false;
+
+    private int pipeWidth;
+    private int pipeHeight;
+    public BufferedImage pipeImage;
+    public BufferedImage flippedPipe;
+
+    public BufferedImage[] cloudImage = new BufferedImage[21];
+    private final int cloudCap = 20;
+    public Cloud[] clouds = new Cloud[cloudCap];
+    private int cloudCount = 0;
+    private final double cloudRate = 0.005;
+
+    public Game()
+    {
+        JFrame frame = new JFrame("Game");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+        frame.setUndecorated(true);
+
+        tk = Toolkit.getDefaultToolkit();
+
+        frame.setVisible(true);
+        frame.requestFocus();
+
+        try {
+            File scoreFile = new File("score.txt");
+            if(!scoreFile.exists())
+            {
+                highScore = 0;
+            } else {
+                try {
+                    FileReader fr = new FileReader(scoreFile);
+                    BufferedReader br = new BufferedReader(fr);
+                    highScore = Integer.parseInt(br.readLine());
+                    br.close();
+                    fr.close();
+                } catch (Exception ex) {
+                    highScore = 0;
+                }
+            }
+
+            bird = new Bird(this, tk);
+
+            BufferedImage image = ImageIO.read(new File("pipe.png"));
+
+            pipeWidth = tk.getScreenSize().width / 16;
+            pipeHeight = (int)(((double)pipeWidth / (double)image.getWidth()) * image.getHeight());
+
+            Image temp = image.getScaledInstance(pipeWidth, pipeHeight, BufferedImage.SCALE_SMOOTH);
+            pipeImage = new BufferedImage(pipeWidth, pipeHeight, BufferedImage.TYPE_INT_ARGB);
+            Graphics g = pipeImage.getGraphics();
+            g.drawImage(temp, 0, 0, null);
+            g.dispose();
+
+            AffineTransform at = new AffineTransform();
+            at.rotate(Math.PI, image.getWidth() / 2, image.getHeight() / 2);
+            AffineTransformOp ato = new AffineTransformOp(at, AffineTransformOp.TYPE_BILINEAR);
+            BufferedImage flipped = ato.filter(image, null);
+
+            temp = flipped.getScaledInstance(pipeWidth, pipeHeight, BufferedImage.SCALE_SMOOTH);
+            flippedPipe = new BufferedImage(pipeWidth, pipeHeight, BufferedImage.TYPE_INT_ARGB);
+            g = flippedPipe.getGraphics();
+            g.drawImage(temp, 0, 0, null);
+            g.dispose();
+
+            Pipe pipe = new Pipe(this, tk, tk.getScreenSize().height / 2, pipeWidth, pipeHeight);
+            pipes[0] = pipe;
+
+            image = ImageIO.read(new File("clouds.png"));
+            int fragHeight = image.getHeight() / 21;
+            for (int i = 0; i < cloudImage.length; i++)
+            {
+                temp = image.getSubimage(0, i * fragHeight, image.getWidth(), fragHeight);
+                cloudImage[i] = new BufferedImage(image.getWidth(), fragHeight, BufferedImage.TYPE_INT_ARGB);
+                g = cloudImage[i].getGraphics();
+                g.drawImage(temp, 0, 0, null);
+                g.dispose();
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.exit(0);
+        }
+
+        canvas = new GameCanvas(this, frame.getGraphics(), tk);
+        frame.add(canvas);
+
+        Thread drawLoop = new Thread(canvas);
+        drawLoop.start();
+
+        frame.addKeyListener(new KeyListener()
+        {
+            @Override
+            public void keyTyped(KeyEvent e)
+            {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e)
+            {
+                if(e.getKeyCode() == KeyEvent.VK_SPACE)
+                {
+                    if (running)
+                        bird.flap();
+                }
+                if(e.getKeyCode() == KeyEvent.VK_ESCAPE)
+                {
+                    running = !running;
+                }
+                if(e.getKeyCode() == KeyEvent.VK_UP)
+                {
+                    if (!running)
+                    {
+                        canvas.cursor--;
+                        canvas.cursor = Math.max(canvas.cursor, 0);
+                    }
+                }
+                if(e.getKeyCode() == KeyEvent.VK_DOWN)
+                {
+                    if (!running)
+                    {
+                        canvas.cursor++;
+                        canvas.cursor = Math.min(canvas.cursor, 6);
+                    }
+                }
+                if(e.getKeyCode() == KeyEvent.VK_RIGHT)
+                {
+                    if (!running)
+                    {
+                        if (canvas.cursor == 2) {
+                            volume += 0.1;
+                            volume = Math.min(volume, 1.0);
+                        }
+                        if (canvas.cursor == 4) {
+                            difficulty += 0.5;
+                            difficulty = Math.min(difficulty, 3.0);
+                        }
+                    }
+                }
+                if(e.getKeyCode() == KeyEvent.VK_LEFT)
+                {
+                    if (!running)
+                    {
+                        if (canvas.cursor == 2) {
+                            volume -= 0.1;
+                            volume = Math.max(volume, 0.0);
+                        }
+                        if (canvas.cursor == 4) {
+                            difficulty -= 0.5;
+                            difficulty = Math.max(difficulty, 0.0);
+                        }
+                    }
+                }
+                if(e.getKeyCode() == KeyEvent.VK_ENTER)
+                {
+                    if (!running)
+                    {
+                        if (canvas.cursor == 0)
+                            reset();
+                        if (canvas.cursor == 1)
+                            System.exit(0);
+                        if (canvas.cursor == 3)
+                            randomGaps = !randomGaps;
+                        if (canvas.cursor == 5)
+                            ramping = !ramping;
+                        if (canvas.cursor == 6)
+                            debug = !debug;
+                    }
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e)
+            {
+            }
+        });
+
+        frame.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (e.getButton() == MouseEvent.BUTTON1) {
+                    firing = true;
+                }
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if (e.getButton() == MouseEvent.BUTTON1) {
+                    firing = false;
+                }
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+
+        frame.addMouseMotionListener(new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                mouseX = e.getX();
+                mouseY = e.getY();
+            }
+        });
+
+        frame.addMouseWheelListener(new MouseWheelListener() {
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e) {
+
+            }
+        });
+    }
+
+    @Override
+    public void run()
+    {
+        while(true)
+        {
+            long startTime = System.nanoTime();
+
+            if (running)
+            {
+                fireCounter -= 1;
+                fireCounter = Math.max(fireCounter, 0);
+                if (firing && fireCounter == 0)
+                {
+                    // spawn new bullet and add to bullet array
+                    fireCounter = 10;
+                }
+                // for each bullet within drawable space:
+                //   perform update
+                //   check for collisions
+                //   if collide():
+                //     do something
+
+                if (Math.random() < cloudRate)
+                {
+                    cloudCount++;
+                    if (cloudCount >= clouds.length)
+                        cloudCount = 0;
+                    if (clouds[cloudCount] == null || clouds[cloudCount].passed)
+                    {
+                        Cloud c = new Cloud(this, tk);
+                        clouds[cloudCount] = c;
+                    }
+                }
+                for (int i = 0; i < clouds.length; i++)
+                {
+                    if(clouds[i] != null)
+                        clouds[i].update();
+                }
+
+                bird.update();
+                for (int i = 0; i < pipes.length; i++) {
+                    if (pipes[i] == null)
+                        continue;
+
+                    if (pipes[i].update()) {
+                        score += 1;
+
+                        if (ramping && score % 10 == 0)
+                        {
+                            difficulty += 0.5;
+                            difficulty = Math.min(difficulty, 3.0);
+                        }
+
+                        if (score > highScore)
+                        {
+                            new Thread(() -> {
+                                highScore = score;
+                                running = true;
+                                try {
+                                    File scoreFile = new File("score.txt");
+                                    PrintWriter pw = new PrintWriter(scoreFile);
+                                    pw.write(String.format("%d%n", highScore));
+                                    pw.close();
+                                } catch (Exception ex) {
+                                    ex.printStackTrace();
+                                }
+                            }).start();
+                        }
+
+                        new Thread(() -> {
+                            try {
+                                AudioInputStream ais = AudioSystem.getAudioInputStream(new File("score.wav").getAbsoluteFile());
+                                Clip clip = AudioSystem.getClip();
+                                clip.open(ais);
+                                FloatControl gain = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+                                gain.setValue(20f * (float) Math.log10(volume));
+                                clip.start();
+                            } catch (Exception ex) {
+                                ex.printStackTrace();
+                            }
+                        }).start();
+                    }
+
+                    if (pipes[i].spawnable && pipes[i].xPos < 3 * tk.getScreenSize().width / 4) {
+                        pipes[i].spawnable = false;
+                        int min = tk.getScreenSize().height / 4;
+                        int range = min * 2;
+                        int y = (int) (Math.random() * range) + min;
+                        Pipe pipe = new Pipe(this, tk, y, pipeWidth, pipeHeight);
+                        pipes[pipeCount] = pipe;
+                        pipeCount++;
+                        if (pipeCount >= pipes.length)
+                            pipeCount = 0;
+                    }
+
+                    if (bird.collide(pipes[i])) {
+                        running = !running;
+                    }
+                }
+            }
+
+            long sleep = (long) waitTime - (System.nanoTime() - startTime) / 1000000;
+            rate = 1000.0 / Math.max(waitTime - sleep, waitTime);
+
+            try
+            {
+                Thread.sleep(Math.max(sleep, 0));
+            } catch (InterruptedException ex)
+            {
+            }
+        }
+    }
+
+    public void reset()
+    {
+        if (score > highScore)
+        {
+            highScore = score;
+            running = true;
+            try {
+                File scoreFile = new File("score.txt");
+                PrintWriter pw = new PrintWriter(scoreFile);
+                pw.write(String.format("%d%n", highScore));
+                pw.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        bird.reset();
+        pipes = new Pipe[5];
+        Pipe p = new Pipe(this, tk, tk.getScreenSize().height / 2, pipeWidth, pipeHeight);
+        pipes[0] = p;
+        pipeCount = 1;
+        score = 0;
+
+        clouds = new Cloud[cloudCap];
+        cloudCount = 0;
+
+        running = true;
+    }
+
+    public static void main(String[] args)
+    {
+        Game game = new Game();
+
+        Thread logicLoop = new Thread(game);
+        logicLoop.start();
+    }
+}
---
+++ src/GameCanvas.java
@@ -0,0 +1,112 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class GameCanvas extends JPanel implements Runnable
+{
+
+    private Game game;
+    private Graphics graphics;
+    Toolkit tk;
+
+    private final double rateTarget = 60.0;
+    private double waitTime = 1000.0 / rateTarget;
+    private double rate = 1000.0 / waitTime;
+
+    public int cursor = 0;
+    public int crosshairSize = 30;
+
+    public GameCanvas(Game game, Graphics g, Toolkit tk)
+    {
+        this.game = game;
+        graphics = g;
+        this.tk = tk;
+    }
+
+    @Override
+    public void run() {
+        while(true)
+        {
+            long startTime = System.nanoTime();
+
+            int width = tk.getScreenSize().width;
+            int height = tk.getScreenSize().height;
+
+            BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2d = image.createGraphics();
+
+            g2d.setColor(Color.CYAN);
+            g2d.fillRect(0, 0, width, height);
+
+            for (int i = 0; i < game.clouds.length; i++)
+            {
+                if (game.clouds[i] != null && !game.clouds[i].passed)
+                    game.clouds[i].drawCloud(g2d);
+            }
+
+            game.bird.drawBird(g2d);
+
+            g2d.setColor(Color.RED);
+            g2d.drawOval(game.mouseX - crosshairSize, game.mouseY - crosshairSize, crosshairSize*2, crosshairSize*2);
+            g2d.drawLine(game.mouseX - crosshairSize, game.mouseY, game.mouseX+crosshairSize, game.mouseY);
+            g2d.drawLine(game.mouseX, game.mouseY-crosshairSize, game.mouseX, game.mouseY+crosshairSize);
+
+            for (int i = 0; i < game.pipes.length; i++)
+            {
+                if (game.pipes[i] != null)
+                    game.pipes[i].drawPipe(g2d);
+            }
+
+            g2d.setColor(Color.BLACK);
+            if (game.running) {
+                g2d.drawString(String.format("Score: %d", game.score), 25, 25);
+                g2d.drawString(String.format("High Score: %d", game.highScore), 25, 50);
+            } else {
+                g2d.drawString(String.format("%s Reset Game", cursor == 0 ? ">" : " "), 25, 25);
+                g2d.drawString(String.format("%s Exit Game", cursor == 1 ? ">" : " "), 25, 50);
+                String vol = "";
+                for (int i = 0; i < 11; i++)
+                {
+                    if ((int) (game.volume * 10) == i)
+                    {
+                        vol += "|";
+                    } else {
+                        vol += "-";
+                    }
+                }
+                g2d.drawString(String.format("%s Volume %s", cursor == 2 ? ">" : " ", vol), 25, 75);
+                g2d.drawString(String.format("%s Randomize Gaps %s", cursor == 3 ? ">" : " ", game.randomGaps ? "(ON)" : "(OFF)"), 25, 100);
+                String dif = "";
+                for (double i = 0.0; i <= 3.0; i+= 0.5)
+                {
+                    if (game.difficulty == i)
+                    {
+                        dif += "|";
+                    } else {
+                        dif += "-";
+                    }
+                }
+                g2d.drawString(String.format("%s Difficulty %s", cursor == 4 ? ">" : " ", dif), 25, 125);
+                g2d.drawString(String.format("%s Ramping %s", cursor == 5 ? ">" : " ", game.ramping ? "(ON)" : "(OFF)"), 25, 150);
+                g2d.drawString(String.format("%s Debug Mode %s", cursor == 6 ? ">" : " ", game.debug ? "(ON)" : "(OFF)"), 25, 175);
+            }
+            if (game.debug) {
+                g2d.drawString(String.format("FPS = %.1f", rate), 200, 25);
+                g2d.drawString(String.format("UPS = %.1f", game.rate), 200, 50);
+            }
+
+            graphics.drawImage(image, 0, 0, null);
+
+            long sleep = (long) waitTime - (System.nanoTime() - startTime) / 1000000;
+            rate = 1000.0 / Math.max(waitTime - sleep, waitTime);
+
+            try
+            {
+                Thread.sleep(Math.max(sleep, 0));
+            } catch (InterruptedException ex)
+            {
+
+            }
+        }
+    }
+}
